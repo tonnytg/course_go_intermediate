e849acf56c21cead6bd83799422b33180f4a89e4
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-05-21 20:15:05 -0300

    feat: adicionando json Marshal

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
+
+type Users []struct {
+	CreatedAt time.Time `json:"createdAt"`
+	Name      string    `json:"name"`
+	Avatar    string    `json:"avatar"`
+	Age       string    `json:"Age"`
+	ID        string    `json:"id"`
+}
 
 func main() {
 	fmt.Println("Request")
@@ -17,4 +27,13 @@ func main() {
 
 	body, err := io.ReadAll(resp.Body)
 	fmt.Printf("Body: %s\n", body)
+
+	var users Users
+	err = json.Unmarshal(body, &users)
+	if err != nil {
+		fmt.Println("Deu erro ao converter")
+	}
+	fmt.Println(users[0].Name)
+	fmt.Println(users[1].Name)
+	fmt.Println(users[2].Name)
 }
